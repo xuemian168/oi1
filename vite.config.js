944391af66e8479948f8d50d6fa11233c8fda2e5
xuemian <xuemian888@gmail.com>
2025-08-24 14:34:02 +1000

    Update Vite configuration to set the base path to '/' for all environments. This change simplifies the deployment process by ensuring consistent asset loading regardless of the environment, enhancing the overall application behavior.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/oi1/' : '/',
+  base: '/',
   build: {
     outDir: 'dist',
     sourcemap: false,
